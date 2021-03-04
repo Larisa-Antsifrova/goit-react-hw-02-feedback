d31da9bf14c1e10f5251703843ca70bc15d4481d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-05 00:58:56 +0200

    Added initial stats class component markup.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <h1>Espresso Cafe Stats</h1>
+        <p>Leave your feedback</p>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        <p>Stats</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </>
+    );
+  }
 }
 
 export default App;
