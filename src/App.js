aa6e4459e0a97e4534f29d25e285fc2c7a429d51
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-06 10:35:16 +0200

    Added methods to calculate total and positive percentage.

--- src/App.js
+++ src/App.js
@@ -7,33 +7,47 @@ class App extends Component {
     bad: 0,
   };
 
-  registerFeedback = event => {
+  countFeedback = event => {
     const { id } = event.target;
     this.setState(prevState => ({
       [id]: prevState[id] + 1,
     }));
   };
 
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => acc + value);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback()
+      ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
+      : 0;
+  };
+
   render() {
     const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
 
     return (
       <>
         <h1>Espresso Cafe Stats</h1>
         <p>Leave your feedback</p>
-        <button id="good" onClick={this.registerFeedback}>
+        <button id="good" onClick={this.countFeedback}>
           Good
         </button>
-        <button id="neutral" onClick={this.registerFeedback}>
+        <button id="neutral" onClick={this.countFeedback}>
           Neutral
         </button>
-        <button id="bad" onClick={this.registerFeedback}>
+        <button id="bad" onClick={this.countFeedback}>
           Bad
         </button>
         <p>Stats</p>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positivePercentage}%</p>
       </>
     );
   }
