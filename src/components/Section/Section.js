62f153b0f480133e624e54f5a2be2c0e66a07d80
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-06 13:19:52 +0200

    Added render condition.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -5,7 +5,7 @@ const Section = ({ title, children }) => {
   return (
     <section>
       <div className="container">
-        <h2>{title}</h2>
+        {title && <h2>{title}</h2>}
         {children}
       </div>
     </section>
